a4d833f36ab9bc2e8491e1129c9235ea725f8403
milan.sobhashna <milan.sobhashna@agileinfoways.com>
2020-12-11 15:28:40 +0530

    done

--- app/src/main/java/com/da/myapplication/MainActivity.kt
+++ app/src/main/java/com/da/myapplication/MainActivity.kt
@@ -12,52 +12,9 @@ import rx.subscriptions.CompositeSubscription
 
 class MainActivity : AppCompatActivity() {
 
-    var dataList = arrayListOf<Category>()
-
-    val api = ApiConfig("http://ayurvedchikitsa.xperttech.in/")
-    val mCompositeSubscription: CompositeSubscription? = CompositeSubscription()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        SendData()
-
     }
 
-    override fun onDestroy() {
-        mCompositeSubscription!!.clear()
-        super.onDestroy()
-    }
-
-    private fun SendData() {
-
-        val call = applySchedulers(this@MainActivity)
-        val apiObservable: Observable<CategoryList> =
-            api.getClientWithHeader("token")!!.create(API::class.java).categoryApi(10, 0)
-        val loginchk: Observable<CategoryList> =
-            call.call(apiObservable).compose(call.applySchedulers())
-
-        val subscription = loginchk.subscribe(object : Observer<CategoryList> {
-            override fun onError(e: Throwable?) {
-            }
-
-            override fun onNext(t: CategoryList?) {
-                dataList = t!!.data
-                Log.d("testuser", dataList.toString())
-                rv_add_new_friend.layoutManager = LinearLayoutManager(this@MainActivity)
-                rv_add_new_friend.adapter = AddNewFriendAdapter(
-                    this@MainActivity,
-                    dataList
-                )
-            }
-
-            override fun onCompleted() {
-
-            }
-
-        })
-
-        mCompositeSubscription!!.add(subscription)
-
-    }
 }
